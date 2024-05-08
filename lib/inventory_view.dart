9390e8121108c3e900e473753d03fe30f70cb95b
Timothy Spinney <tspinney@seattleu.edu>
2024-05-08 09:56:29 -0700

    removing commented out code

--- lib/inventory_view.dart
+++ lib/inventory_view.dart
@@ -27,28 +27,6 @@ class InventoryView extends StatelessWidget {
         return const CircularProgressIndicator();
       }
     );
-    //final locations = context.select((Inventory inventory) => inventory.locations);
-    /*return ListView.builder(
-      itemCount: locations.length,
-      itemBuilder: (context, index) {
-        final location = locations.elementAt(index);
-        return Card(
-          child: ExpansionTile(
-            initiallyExpanded: true,
-            key: PageStorageKey('expansion-$location'),
-            title: Text(location),
-            children: [
-              const Divider(),
-              InventoryLocationView(
-                // We need this key here ore else the PageStorageKey for ExpansionTile will overwrite the scroll position with isExpanded
-                key: PageStorageKey('location-$location'),
-                location: location,
-              ),
-            ],
-          ),
-        );
-      },
-    );*/
   }
 
   Stream<List<InventoryItem>> readInventoryItems() => FirebaseFirestore.instance
